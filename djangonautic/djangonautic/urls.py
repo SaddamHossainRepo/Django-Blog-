2c4eb762819e8251de10ee273c600d60d18a9bf4
Saddam Hossain <saddam13@cse.pstu.ac.bd>
2020-10-17 14:55:20 +0600

    A blog with django

---
+++ djangonautic/djangonautic/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', views.homepage),
+    path('about/', views.about),
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
